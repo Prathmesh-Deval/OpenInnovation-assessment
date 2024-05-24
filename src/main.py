9f91223104bb12979cffc40bfd011d52af64b17f
prathmesh1deval <prathmesh1deval@tataaig.com>
2024-05-24 14:02:05 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import os
+import json
+
+
+def create_flask_app() -> Flask:
+    app = Flask(__name__)
+    app.config['CORS_HEADERS'] = 'application/json'
+    app.secret_key = b'secret key'
+    app.config.from_file('env_vars.json',json.load,silent=False)
+    for i in app.config:
+        os.environ[i] = str(app.config[i])
+    return app
+
+
+def init_api(flask_app: Flask) -> Flask:
+    from apis import rest_api
+    rest_api.init_app(flask_app)
+    return flask_app
+
+
+flask_app = create_flask_app()
+flask_app = init_api(flask_app)
+
+if __name__ == "__main__":
+    flask_app.run()
---
+++ src/services/database.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sqlite3
+
+resized_df = pd.read_csv('/Users/prdeval/Desktop/Projects/openinnovation/src/data/resized_image_data.csv')
+
+conn = sqlite3.connect('image_data.db')
+cursor = conn.cursor()
+
+columns = ['depth REAL'] + [f'col{i+1} REAL' for i in range(150)]
+create_table_query = f"CREATE TABLE IF NOT EXISTS resized_images ({', '.join(columns)});"
+
+cursor.execute(create_table_query)
+resized_df.to_sql('resized_images', conn, if_exists='replace', index=False)
+
+# Commit the changes and close the connection
+conn.commit()
+print("*****")
+query = "SELECT * FROM resized_images"
+df_from_db = pd.read_sql_query(query, conn)
+conn.close()
+
+print("Data read from database:")
+print(df_from_db)
